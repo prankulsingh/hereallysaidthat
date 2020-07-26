c1912bd7ab848b2aa2e5ddf008a40f0f2749b876
Prankul Singh <prankul.singh@airtel.com>
2020-07-26 18:47:30 +0530

    Updates

---
+++ precache-manifest.d5a918174d7096700ff8de5ae24aca95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e2573f605f3b06f850c98cb1adba15",
+    "url": "/hereallysaidthat/index.html"
+  },
+  {
+    "revision": "a3edb5b74b63d45717d4",
+    "url": "/hereallysaidthat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb25fdc8a58a1fc9546e",
+    "url": "/hereallysaidthat/static/js/2.09a9d2e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hereallysaidthat/static/js/2.09a9d2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3edb5b74b63d45717d4",
+    "url": "/hereallysaidthat/static/js/main.5d5b4bfe.chunk.js"
+  },
+  {
+    "revision": "e7d6e47c62360718f57f",
+    "url": "/hereallysaidthat/static/js/runtime-main.70cae730.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hereallysaidthat/static/media/logo.5d5d9eef.svg"
+  }
+]);
